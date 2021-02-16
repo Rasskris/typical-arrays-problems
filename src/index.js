a0170ac7e3f2371f66824aad600a3c9a97cfaa51
Rasskris <rassoshenko.kristina@yandex.ru>
2021-02-16 18:26:53 +0300

    feat: add solutions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 
 exports.min = function min (array) {
-  return 0;
+  return (!array || array.length < 1) ? 0 : Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  return (!array || array.length < 1) ? 0 : Math.max(...array);;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length < 1) {
+    return 0;
+  }
+  const sum = array.reduce((acc, item) => acc += item, 0);
+  return sum / array.length;
 }
